ecee6bb683b9a5e77c789972eb740375436562c4
IvanMinev9 <i.minev.m@gmail.com>
2023-09-24 15:01:39 +0300

    Updated all tasks

---
+++ 5. Basic Syntax, Conditional Statements and Loops/foreign-languages.js
@@ -0,0 +1,14 @@
+function language(country) {
+  if (country == `USA` || country == `England`) {
+    console.log(`English`);
+  } else if (
+    country == `Spain` ||
+    country == `Argentina` ||
+    country == `Mexico`
+  ) {
+    console.log(`Spanish`);
+  } else {
+    console.log(`unknown`);
+  }
+}
+language(`Spain`);
