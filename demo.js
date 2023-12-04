e17a2d8c45fa19bbb740579ea0d9556cf31c6aec
IvanMinev9 <i.minev.m@gmail.com>
2023-12-04 16:57:34 +0200

    JS

--- demo.js
+++ demo.js
@@ -1,14 +1,37 @@
-function solve(number, arr) {
-  let newArray = [];
+function processOrders(input) {
+  let totalIncome = 0;
 
-  for (let i = 0; i <= number.length; i++) {
-    newArray.push(arr[i]);
-  }
-  let reversedArray = [];
+  for (const line of input) {
+    const validOrderRegex =
+      /%(?<customer>[A-Z][a-z]+)%[^|$%.]*<(?<product>\w+)>[^|$%.]*\|(?<count>\d+)\|[^|$%.0-9]*(?<price>\d+\.?\d*)\$/gm;
+    const match = line.match(validOrderRegex);
 
-  for (let i = number - 1; i >= 0; i--) {
-    reversedArray[reversedArray.length] = newArray[i];
+    if (match) {
+      const { customer, product, count, price } = match.groups;
+      const totalPrice = Number(count) * Number(price);
+      totalIncome += totalPrice;
+
+      console.log(`${customer}: ${product} - ${totalPrice.toFixed(2)}`);
+    }
   }
-  console.log(reversedArray.join(` `));
+
+  console.log(`Total income: ${totalIncome.toFixed(2)}`);
 }
-solve(3, [10, 20, 30, 40, 50]);
+
+// Example usage
+const input1 = [
+  "%George%<Croissant>|2|10.3$",
+  "%Peter%<Gum>|1|1.3$",
+  "%Maria%<Cola>|1|2.4$",
+  "end of shift",
+];
+processOrders(input1);
+
+const input2 = [
+  "%InvalidName%<Croissant>|2|10.3$",
+  "%Peter%<Gum>1.3$",
+  "%Maria%<Cola>|1|2.4$",
+  "%Valid%<Valid>valid|10|valid20$",
+  "end of shift",
+];
+processOrders(input2);
