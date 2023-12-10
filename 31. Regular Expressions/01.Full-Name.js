6ae4697bae89a0f7b16b9ee5e22b808454dce34a
IvanMinev9 <i.minev.m@gmail.com>
2023-12-10 14:35:07 +0200

    RegEx JS

--- 31. Regular Expressions/01.Full-Name.js
+++ 31. Regular Expressions/01.Full-Name.js
@@ -1,4 +1,10 @@
-function matchName(arr) {}
+function matchName(arr) {
+  let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
+
+  let result = arr.match(pattern);
+
+  console.log(result.join(` `));
+}
 matchName(
-	"Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"
+  "Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov"
 );
