943c6a0ac386e1cc5fb0c5c7b80b1da095cbe25e
IvanMinev9 <i.minev.m@gmail.com>
2024-02-07 18:44:05 +0200

    js

--- 14. Functions/Calc.js
+++ 14. Functions/Calc.js
@@ -1,26 +1,3 @@
-function solve(a, b, operator) {
-	let multiply = (a, b) => a * b;
-	let divide = (a, b) => a / b;
-	let add = (a, b) => a + b;
-	let subtract = (a, b) => a - b;
-
-	let operations = [multiply, divide, add, subtract];
-
-	let operationsName = [`multiply`, `divide`, `add`, `subtract`];
-
-	let index;
-
-	for (i = 0; i < operationsName.length; i++) {
-		if (operationsName[i] == operator) {
-			index = i;
-		}
-	}
-
-	let oper = operations[index];
-	console.log(oper(a, b));
-}
-
-solve(5, 5, `multiply`);
-solve(40, 8, `divide`);
-solve(12, 19, `add`);
-solve(50, 13, `subtract`);
+(a,b, operator) =>({
+    `add`: (a,b) => a+b
+}[operator](a,b))
