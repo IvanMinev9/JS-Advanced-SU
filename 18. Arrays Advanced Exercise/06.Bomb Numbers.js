3ac6062f2da44ac69895662f560a833af9564833
IvanMinev9 <i.minev.m@gmail.com>
2024-02-26 20:13:47 +0200

    Js-update

--- 18. Arrays Advanced Exercise/06.Bomb Numbers.js
+++ 18. Arrays Advanced Exercise/06.Bomb Numbers.js
@@ -1,4 +1,16 @@
-function bomb(numbers, bombInfo) {}
+function bomb(numbers, bombInfo) {
+  let [bobmNum, power] = bombInfo;
+
+  while (numbers.includes(bobmNum)) {
+    let idx = numbers.indexOf(bobmNum);
+    numbers.splice(Math.max(0, idx - power), power * 2 + 1, 0);
+  }
+  let sum = 0;
+  for (let num of numbers) {
+    sum += num;
+  }
+  console.log(sum);
+}
 bomb([1, 2, 2, 4, 2, 2, 2, 9], [4, 2]);
 bomb([1, 4, 4, 2, 8, 9, 1], [9, 3]);
 bomb([1, 7, 7, 1, 2, 3], [7, 1]);
