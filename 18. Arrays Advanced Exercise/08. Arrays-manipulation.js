4262d3b89e18d47a48d331624e56178e9edb2d4f
IvanMinev9 <i.minev.m@gmail.com>
2024-03-02 19:15:50 +0200

    jsUpd-m

--- 18. Arrays Advanced Exercise/08. Arrays-manipulation.js
+++ 18. Arrays Advanced Exercise/08. Arrays-manipulation.js
@@ -1,26 +1,47 @@
 function manipulation(nums, executions) {
-	for (let command of executions) {
-		let tokens = command.split(` `);
+	for (let eccxx of executions) {
+		let tokens = eccxx.split(` `);
 		let action = tokens[0];
 
 		if (action == `add`) {
 			let idx = Number(tokens[1]);
 			let num = Number(tokens[2]);
-
 			nums.splice(idx, 0, num);
 		} else if (action == `addMany`) {
 			let idx = Number(tokens[1]);
-			let numsToAdd = tokens.slice(2);
+			let contemptOfNums = tokens.slice(2);
 
-			for (let num of numsToAdd) {
+			for (let num of contemptOfNums) {
 				nums.splice(idx, 0, num);
+				idx++;
 			}
 		} else if (action == `contains`) {
 			let num = Number(tokens[1]);
-
 			let idx = nums.indexOf(num);
-
 			console.log(idx);
+		} else if (action == `remove`) {
+			let idx = Number(tokens[1]);
+
+			nums.splice(idx, 1);
+		} else if (action == `shift`) {
+			let rot = Number(tokens[1]);
+
+			for (let i = 1; i <= rot; i++) {
+				let el = nums.shift();
+				nums.push(el);
+			}
+		} else if (action == `sumPairs`) {
+			let paired = [];
+			for (let i = 0; i < nums.length; i += 2) {
+				if (i + 1 < nums.length) {
+					paired.push(nums[i] + nums[i + 1]);
+				} else {
+					paired.push(nums[i]);
+				}
+			}
+			nums = paired;
+		} else if (action == `print`) {
+			console.log(`[${nums.join(`, `)}]`);
 		}
 	}
 }
