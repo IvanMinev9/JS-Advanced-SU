9d2df70297d4d02d255fe253fb5069e6023981b2
IvanMinev9 <i.minev.m@gmail.com>
2024-03-16 09:47:26 +0200

    JS

---
+++ 20. Exam Preparation/Computer Store.js
@@ -0,0 +1,48 @@
+function compStor(arr) {
+	let priceNoTax = 0;
+	let command = arr.shift();
+
+	while (command != `regular` && command != `special`) {
+		let price = Number(command);
+
+		if (price <= 0) {
+			console.log(`Invalid price!`);
+			command = arr.shift();
+			continue;
+		}
+		priceNoTax += price;
+
+		command = arr.shift();
+	}
+	if (priceNoTax == 0) {
+		console.log(`Invalid order!`);
+	} else {
+		let tax = 0.2 * priceNoTax;
+		let priceWithTax = priceNoTax + tax;
+
+		if (command == `special`) {
+			priceWithTax *= 0.9;
+		}
+		console.log(`Congratulations you\'ve just bought a new computer!`);
+		console.log(`Price without taxes: ${priceNoTax.toFixed(2)}$`);
+		console.log(`Taxes: ${tax.toFixed(2)}$`);
+		console.log(`-----------`);
+		console.log(`Total price: ${priceWithTax.toFixed(2)}$`);
+	}
+}
+compStor(["1050", "200", "450", "2", "18.50", "16.86", "special"]);
+
+console.log(`-----------`);
+compStor([
+	"1023",
+	"15",
+	"-20",
+	"-5.50",
+	"450",
+	"20",
+	"17.66",
+	"19.30",
+	"regular",
+]);
+console.log(`----------`);
+compStor(["regular"]);
